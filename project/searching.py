e9a914c3545e9251be34df9dfe55dbe13d2a98c1
Brian G <brian.gregorius@gmail.com>
2019-02-18 15:57:45 -0500

    copleted binary search recursive

--- project/searching.py
+++ project/searching.py
@@ -1,33 +1,40 @@
-# STRETCH: implement Linear Search				
+# STRETCH: implement Linear Search
 def linear_search(arr, target):
-  for indx, num in enumerate(arr):
-    if num == x:
-      print(indx, num)
-  
-  # TO-DO: add missing code
+    for indx, num in enumerate(arr):
+        if num == target:
+            print(indx)
 
-   return -1   # not found
+    return -1   # not found
 
 
-# STRETCH: write an iterative implementation of Binary Search 
+# STRETCH: write an iterative implementation of Binary Search
 def binary_search(arr, target):
 
-  if len(arr) == 0:
-    return -1 # array empty
-    
-  low = 0
-  high = len(arr)-1
+    if len(arr) == 0:
+        return -1  # array empty
 
-  # TO-DO: add missing code
+    low = 0
+    high = len(arr)-1
 
-  return -1 # not found
+    # TO-DO: add missing code
+
+    return -1  # not found
 
 
-# STRETCH: write a recursive implementation of Binary Search 
-def binary_search_recursive(arr, target, low, high):
-  
-  middle = (low+high)/2
+# STRETCH: write a recursive implementation of Binary Search
+def binary_search_recursive(arr, target):
 
-  if len(arr) == 0:
-    return -1 # array empty
-  # TO-DO: add missing if/else statements, recursive calls
+    middle = len(arr)//2
+
+    if len(arr) == 0:
+        return -1  # array empty
+    # TO-DO: add missing if/else statements, recursive calls
+    else:
+        if target == arr[middle]:
+            return target
+        elif target < arr[middle]:
+            binary_search_recursive(arr[:middle], target)
+        elif target > arr[middle]:
+            binary_search_recursive(arr[middle:], target)
+
+    return arr.index(target)
